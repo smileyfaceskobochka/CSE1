b0c5a0e2f1b3774db751c601c375db5d6f3095b4
smileyfaceskobochka <cherkasov.aleksandr.work@yandex.ru>
2024-10-26 17:58:26 +0300

    excluding exe files + 4.c

--- labs/CompSCi/CompSciLab3/code/4.c
+++ labs/CompSCi/CompSciLab3/code/4.c
@@ -3,50 +3,51 @@
 
 int n;
 
-// Функция для нахождения обратного по модулю числа https://ru.algorithmica.org/cs/modular/reciprocal/
-int modInverse(int a, int m) {
-    int m0 = m, t, q;
-    int x0 = 0, x1 = 1;
+// Функция для нахождения обратного по модулю числа (Разобраться в алгоритме)
+int modMultInverse(int a, int m) {
+    int m0 = m;
+    int y = 0, x = 1;
 
-    if (m == 1) return 0;
+    if (m == 1)
+        return 0;
 
-    // Используя расширенный алгоритм Евклида
     while (a > 1) {
-        q = a / m;
-        t = m;
-        
-        m = a % m;
-        a = t;
-        t = x0;
+        int q = a / m;
+        int t = m;
 
-        x0 = x1 - q * x0;
-        x1 = t;
+        m = a % m, a = t;
+        t = y;
+
+        // Update y and x
+        y = x - q * y;
+        x = t;
     }
 
-    // Make x1 positive
-    if (x1 < 0) x1 += m0;
+    // 
+    if (x < 0)
+        x += m0;
 
-    return x1;
+    return x;
 }
 
-// Функция для преобразования числа из СОК в десятичную СС (ост, базисы)
+// Функция для преобразования числа из СОК в десятичную СС (ост, базисы) https://www.ispras.ru/proceedings/docs/2024/36/4/isp_36_2024_4_117.pdf
 int residueToDec(int residues[], int moduli[]){
-    int M = 1; // Произведение базисов
-    int X = 0; // Число в 10СС
+    int prod = 1; // Произведение модулей
+    int res = 0;  // Результат в десятичной системе
 
-    // Вычисление произведения базисов
+    // Вычисление произведения модулей
     for (int i = 0; i < n; i++) {
-        M *= moduli[i];
+        prod *= moduli[i];
     }
 
-    // Число в 10СС
+    // Вычисление результата в десятичной системе
     for (int i = 0; i < n; i++) {
-        int Mi = M / moduli[i]; // Частичное произведение
-        int yi = modInverse(Mi, moduli[i]); // Обратное по модулю число
-        X += residues[i] * Mi * yi;
+        int partProd = prod / moduli[i]; // Частичное произведение
+        int inv = modMultInverse(partProd, moduli[i]); // Обратное по модулю число
+        res += residues[i] * partProd * inv;
     }
 
-    return X % M; // Результат по модулю M
+    return res % prod; // Результат по модулю product
 }
 
 // Функция для сложения чисел в системе остаточных классов (базисы, ост1, ост2)
@@ -67,15 +68,25 @@ int writeToArr(int arr[]) {
     }
     return 0;
 }
-/*
-int printArr(int arr[]){
 
+int exitWithErr(int moduli[], int residues0[], int residues1[]) {
+    int range = 1; // находим диапазон +1
     for (int i = 0; i < n; i++) {
-        printf("%d", arr[i]);
+        range *= moduli[i];
     }
-    printf(" ");
+    
+    // Преобразуем остатки в десятичное число
+    int decimalValue0 = residueToDec(residues0, moduli);
+    int decimalValue1 = residueToDec(residues1, moduli);
+
+    int totValue = decimalValue0 + decimalValue1;
+    if (totValue < 0 || totValue >= range) {
+        return -1; // Если сумма выходит за пределы диапазона, возвращаем -1
+    }
+
+    return 0;
 }
-*/
+
 int main() {
     scanf("%d", &n);
 
@@ -88,7 +99,7 @@ int main() {
     writeToArr(residues1);
 
     int resVal = calcSum(moduli, residues0, residues1);
-    if (resVal == -1) {
+    if (exitWithErr(moduli, residues0, residues1) == -1) {
         printf("-1\n");
     } else {
         printf("%d\n", resVal);
