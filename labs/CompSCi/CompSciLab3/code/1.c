c9d52cc56683a9e8a08132712eb00ef547a10679
smileyfaceskobochka <cherkasov.aleksandr.work@yandex.ru>
2024-10-29 05:45:48 +0300

    чучут

--- labs/CompSCi/CompSciLab3/code/1.c
+++ labs/CompSCi/CompSciLab3/code/1.c
@@ -2,7 +2,6 @@
 
 int cntBin(int n){
     int i = 0; //Счетчик нулей в 2-ой записи числа n
-    
     do {
         if (n % 2 == 0){
             i++;
@@ -16,7 +15,7 @@ int main(){
     int n; 
     scanf("%d", &n); //Ввод числа n
     if(n < 0){
-        return 1;
+        return -1;
     } //Вывод ошибки
     cntBin(n);
     return 0;
