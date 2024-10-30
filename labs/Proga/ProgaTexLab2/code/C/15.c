edb4d7137cab1252de2499c0ceb5158079d99a82
smileyfaceskobochka <cherkasov.aleksandr.work@yandex.ru>
2024-10-30 18:04:47 +0300

    Лекции теперь записываю в md

--- labs/Proga/ProgaTexLab2/code/C/15.c
+++ labs/Proga/ProgaTexLab2/code/C/15.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-    
+    int n;
+    scanf("%d", &n);
 
+    int *arr = (int*)malloc(sizeof(int)*n);
+
+    for (int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    
 }
